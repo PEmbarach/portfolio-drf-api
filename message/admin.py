acef95d30da20c1c22f2ef219ca19c4e5e6f71a4
Pablo Embarach <pabloembarach@gmail.com>
2023-08-22 11:31:23 +0100

    Create superuser

--- message/admin.py
+++ message/admin.py
@@ -4,7 +4,7 @@ from .models import message
 class MessageAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'email', 'company', 'message')
     list_display_links = ('id', 'name')
-    search_fields = ('name', 'email', 'comapny')
+    search_fields = ('name', 'email', 'company')
     list_per_page = 25
 
 admin.site.register(message, MessageAdmin)
