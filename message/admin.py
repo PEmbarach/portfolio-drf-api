41d1a9681789d6444a5f7efdd10cb31ca475a640
Pablo Embarach <pabloembarach@gmail.com>
2023-08-22 11:21:23 +0100

    Create admin class at admin.py message

--- message/admin.py
+++ message/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import message
 
-# Register your models here.
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'company', 'message')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', 'email', 'comapny')
+    list_per_page = 25
+
+admin.site.register(message, MessageAdmin)
